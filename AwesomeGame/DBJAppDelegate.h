cc739bf5cfb45a5d5b219af3931968376d706ecb
David Jordan <dbj8@cornell.edu>
2014-06-18 14:18:29 -0500

    Initial Commit

---
+++ AwesomeGame/DBJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DBJAppDelegate.h
+//  AwesomeGame
+//
+//  Created by David Jordan on 6/18/14.
+//  Copyright (c) 2014 dbjAppIndustries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
